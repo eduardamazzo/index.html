b6605183b1463e258f48648f849cdc249668ee81
eduardamazzo <106194301+eduardamazzo@users.noreply.github.com>
2022-09-28 16:58:09 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,17 @@
-
+// alert("Oi! Eu sou o Alert");
+// document.write("Estou na página.");
+// console.log("Eu estou no console.");
+let tabuada = 2 
+function alo(){
+    document.white( tabuada + " x 1 = " + (tabuada*1)+"br>" );
+    document.white( tabuada + " x 1 = " + (tabuada*1)+"br" );
+    document.white( tabuada + " x 2 = " + (tabuada*2)+"br" );
+    document.white( tabuada + " x 3 = " + (tabuada*3)+"br" );
+    document.white( tabuada + " x 4 = " + (tabuada*4)+"br" );
+    document.white( tabuada + " x 5 = " + (tabuada*5)+"br" );
+    document.white( tabuada + " x 6 = " + (tabuada*6)+"br" );
+    document.white( tabuada + " x 7 = " + (tabuada*7)+"br" );
+    document.white( tabuada + " x 8 = " + (tabuada*8)+"br" );
+    document.white( tabuada + " x 9 = " + (tabuada*9)+"br" );
+    document.white( tabuada + " x 10 = " + (tabuada*10)+"br" );
+}
